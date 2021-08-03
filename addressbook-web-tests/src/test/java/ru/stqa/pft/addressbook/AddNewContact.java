97bf7e28d2584eb77542d907b1b0706457b20164
t.sadovskaya <t.sadovskaya@corp.vk.com>
2021-08-03 20:38:56 +0300

    Задание 4. Изменила аннотации

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/AddNewContact.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/AddNewContact.java
@@ -7,14 +7,14 @@ import org.openqa.selenium.NoAlertPresentException;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class AddNewContact {
   private WebDriver wd;
 
-  @BeforeClass(alwaysRun = true)
+  @BeforeMethod(alwaysRun = true)
   public void setUp() throws Exception {
     wd = new FirefoxDriver();
     wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
@@ -67,7 +67,7 @@ public class AddNewContact {
     wd.findElement(By.linkText("home page")).click();
   }
 
-  @AfterClass(alwaysRun = true)
+  @AfterMethod(alwaysRun = true)
   public void tearDown() throws Exception {
     wd.quit();
   }
