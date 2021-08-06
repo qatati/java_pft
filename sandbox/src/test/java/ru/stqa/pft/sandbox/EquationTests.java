dec47c747f8c6c04424bad3e41864ef6a81029a1
t.sadovskaya <t.sadovskaya@corp.vk.com>
2021-08-06 17:54:55 +0300

    add if-then-else

--- sandbox/src/test/java/ru/stqa/pft/sandbox/EquationTests.java
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/EquationTests.java
@@ -22,4 +22,22 @@ public class EquationTests {
     Equation equation = new Equation(1,5,6);
     Assert.assertEquals(equation.rootNumber(), 2);
   }
+
+  @Test
+  public void testLinear() {
+    Equation equation = new Equation(0,1,1);
+    Assert.assertEquals(equation.rootNumber(), 1);
+  }
+
+  @Test
+  public void testConstant() {
+    Equation equation = new Equation(0,0,1);
+    Assert.assertEquals(equation.rootNumber(), 0);
+  }
+
+  @Test
+  public void testZero() {
+    Equation equation = new Equation(0,0,0);
+    Assert.assertEquals(equation.rootNumber(), -1);
+  }
 }
