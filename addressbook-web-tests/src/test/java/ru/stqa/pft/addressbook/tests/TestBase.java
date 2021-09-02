0239b95251311bd73d64625de09067bebf79ea51
t.sadovskaya <t.sadovskaya@corp.vk.com>
2021-09-02 18:50:00 +0300

    Задание №14: Вынесено описание тестового стенда в конфигурационный файл

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
@@ -1,11 +1,17 @@
 package ru.stqa.pft.addressbook.tests;
 
+import java.lang.reflect.Method;
 import org.openqa.selenium.remote.BrowserType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 import ru.stqa.pft.addressbook.appmanager.ApplicationManager;
 
 public class TestBase {
+  Logger logger = LoggerFactory.getLogger(TestBase.class);
 
   protected static final ApplicationManager app =
       new ApplicationManager(System.getProperty("browser", BrowserType.CHROME));
@@ -18,5 +24,15 @@ public class TestBase {
   @AfterSuite(alwaysRun = true)
   public void tearDown() throws Exception {
     app.stop();
+  }
+
+  @BeforeMethod
+  public void logTestStart(Method m) {
+    logger.info("Start testGroupCreation");
+  }
+
+  @AfterMethod(alwaysRun = true)
+  public void logTestStop(Method m) {
+    logger.info("Stop testGroupCreation");
   }
 }
