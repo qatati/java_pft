c1678fc0004f53e083f1ab7c662ab343e79b5c81
t.sadovskaya <t.sadovskaya@corp.vk.com>
2021-09-09 17:13:25 +0300

    Лекция 7.6

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
@@ -31,8 +31,10 @@ public class ContactHelper extends BaseHelper {
     type(By.name("email3"), contactData.getEmail3());
     attach(By.name("photo"), contactData.getPhoto());
     if (creation) {
-      new Select(wd.findElement(By.name("new_group")))
-//          .selectByVisibleText(contactData.getGroups());
+      if (contactData.getGroups().size() > 0) {
+        Assert.assertTrue(contactData.getGroups().size() == 1);
+      }
+      new Select(wd.findElement(By.name("new_group"))).selectByVisibleText(contactData.getGroups().iterator().next().getName());
     } else {
       Assert.assertFalse(isElementPresent(By.name("new_group")));
     }
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactAddressTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactAddressTests.java
@@ -6,17 +6,19 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
+import ru.stqa.pft.addressbook.model.Groups;
 
 public class ContactAddressTests extends TestBase {
 
   @BeforeMethod
   public void ensurePreconditions() {
+    Groups groups = app.db().groups();
     app.goTo().homePage();
     if (app.contact().list().isEmpty()) {
       app.goTo().newContactPage();
       app.contact().create(
           new ContactData().setFirstname("Татьяна").setLastname("Садовская")
-              .setMobilePhone("8-900-660-60-60").setEmail("qa.tatiana.qa@gmail.com"));
+              .setMobilePhone("8-900-660-60-60").setEmail("qa.tatiana.qa@gmail.com").inGroup(groups.iterator().next()));
 //              .setGroup("test1"));
     }
   }
